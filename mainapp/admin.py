a26229636ec919d0b73fcf138d0473aafea9f3a1
Aleksandr <aleks.prfnk@gmail.com>
2021-09-23 16:47:39 +0300

    add Admin class and register them add 'if' for superuser in base.html(add link to administration panel)

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from mainapp.models import Product, ProductCategory
+from django.contrib.admin import AdminSite
+from users.models import Admin
+
 # Register your models here.
 
 admin.site.register(ProductCategory)
 admin.site.register(Product)
+
+
+class MyAdminSite(AdminSite):
+    site_header = 'Your Admin site'
+
+admin_site = MyAdminSite(name='admin')
+admin_site.register(Admin)
